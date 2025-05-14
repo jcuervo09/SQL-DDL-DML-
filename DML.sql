022cde2f82cd791a907bbc5674cd32bcc7ac51c1
sebatian <juan_sforeroc@soy.sena.edu.co>
2025-05-14 00:32:42 -0500

    actualizacion DML

--- DML.sql
+++ DML.sql
@@ -22,5 +22,54 @@ call agregar_producto("Impresora HP Laserjet Pro M26nw",180,3);
 
 -- JOIN
 -- 1. Lista el nombre de todos los productos que hay en la tabla producto.
-
 select nombre from producto;
+-- 2. Lista los nombres y los precios de todos los productos de la tabla producto.
+SELECT nombre, precio
+FROM producto;
+-- 3. Lista todas las columnas de la tabla producto
+SELECT *
+FROM producto;
+-- 4. Lista el nombre de los productos, el precio en euros y el precio en dólares estadounidenses (USD).
+SELECT
+    nombre,
+    precio AS euros,
+    precio * 1.08 AS USD
+FROM
+    producto;
+-- 5. Lista el nombre de los productos, el precio en euros y el precio en dólares estadounidenses (USD). Utiliza los siguientes alias para las columnas: nombre de producto, euros, dólares
+    SELECT
+    nombre AS "nombre de producto",
+    precio AS euros,
+    precio * 1.08 AS dólares
+FROM
+    producto;
+-- 6. Lista los nombres y los precios de todos los productos de la tabla producto, convirtiendo los nombres a mayúscula.
+SELECT
+    UPPER(nombre) AS nombre_mayuscula,
+    precio
+FROM
+    producto;
+-- 7. Lista los nombres y los precios de todos los productos de la tabla producto, convirtiendo los nombres a minúscula.
+SELECT
+    LOWER(nombre) AS nombre_minuscula,
+    precio
+FROM
+    producto;
+-- 8. Lista el nombre de todos los fabricantes en una columna, y en otra columna obtenga en mayúsculas los dos primeros caracteres del nombre del fabricante.
+SELECT
+    nombre,
+    UPPER(LEFT(nombre, 2)) AS dos_primeras_letras_mayuscula
+FROM
+    fabricante;
+-- 9. Lista los nombres y los precios de todos los productos de la tabla producto, redondeando el valor del precio.
+SELECT
+    nombre,
+    ROUND(precio) AS precio_redondeado
+FROM
+    producto;
+-- 10. Lista los nombres y los precios de todos los productos de la tabla producto, truncando el valor del precio para mostrarlo sin ninguna cifra decimal
+SELECT
+    nombre,
+    TRUNCATE(precio, 0) AS precio_truncado
+FROM
+    producto;
